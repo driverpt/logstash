44a05fc134d751d8002a8fffbdcff2334dea1656
Luís Duarte <lduarte@luisduarte.net>
2016-04-07 16:40:45 +0100

    Small performance improvement
    Arrays.asList() already returns an ArrayList

--- logstash-core-event-java/src/main/java/com/logstash/FieldReference.java
+++ logstash-core-event-java/src/main/java/com/logstash/FieldReference.java
@@ -11,7 +11,7 @@ public class FieldReference {
     private List<String> path;
     private String key;
     private String reference;
-    private static List<String> EMPTY_STRINGS = Arrays.asList("");
+    private static final List<String> EMPTY_STRINGS = Arrays.asList("");
 
     public FieldReference(List<String> path, String key, String reference) {
         this.path = path;
@@ -32,7 +32,7 @@ public class FieldReference {
     }
 
     public static FieldReference parse(String reference) {
-        List<String> path = new ArrayList(Arrays.asList(reference.split("[\\[\\]]")));
+        List<String> path = Arrays.asList(reference.split("[\\[\\]]"));
         path.removeAll(EMPTY_STRINGS);
         String key = path.remove(path.size() - 1);
         return new FieldReference(path, key, reference);
